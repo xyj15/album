cb1fae7c7fa69fa29baeb6b11ec354dc36499c11
juaner <591024170@qq.com>
2017-12-09 15:35:43 +0800

    上传相册

--- routes/onephoto.js
+++ routes/onephoto.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var fs=require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('onephoto');
+});
+router.post('/onephoto', function(req, res) {
+
+    var name=req.body.username;
+    var album=req.body.album;
+    var photo=req.body.photo;
+    var elePath="/photo/"+name+"/"+album+"/"+photo+".jpg";
+    res.json({"path":elePath});
+});
+router.post('/delete', function(req, res) {
+    var name=req.body.username;
+    var album=req.body.album;
+    var photo=req.body.photo;
+    var path="./public/photo/"+name+"/"+album+"/"+photo+".jpg";
+    fs.unlinkSync(path);
+    res.json({"success":"1"});
 });
 module.exports = router;
--- routes/photo.js
+++ routes/photo.js
@@ -1,6 +1,28 @@
 var express = require('express');
 var router = express.Router();
 var fs=require('fs');
+var multer = require('multer');
+
+var storage = multer.diskStorage({
+    //设置上传后文件路径，uploads文件夹会自动创建。
+    destination: function (req, file, cb) {
+
+        var url=(req.headers.referer).split("?")[1];
+        var name=getQueryString(url,"name");
+        var album=getQueryString(url,"album");
+        var path="./public/photo/"+name+"/"+album;
+        var juedui="/Users/juaner/IdeaProjects/album/public/photo/"+name+"/"+album;
+        console.log(juedui);
+        cb(null, juedui);
+    },
+    //给上传文件重命名，获取添加后缀名
+    filename: function (req, file, cb) {
+        cb(null,file.originalname);
+    }
+});
+var upload = multer({
+    storage: storage
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('photo');
@@ -13,7 +35,6 @@ router.post('/photo', function(req, res) {
     var paths=[];
     var names=[];
     var elePath="/photo/"+name+"/"+album+"/";
-    console.log(path);
     var pa = fs.readdirSync(path);
     pa.forEach(function(ele,index){
         if(ele!=".DS_Store") {
@@ -35,13 +56,20 @@ router.post('/del', function(req, res) {
     var pa = fs.readdirSync(path);
     pa.forEach(function(ele,index){
         fs.unlinkSync(path+ele);
-        console.log(path+ele);
     })
     fs.unlinkSync(cover);
     fs.rmdirSync(path);
     res.json({"success":"1"});
 
-
-
 });
+router.post('/upload',upload.single('recfile'),function(req,res){
+    res.json({"success":"1"});
+});
+function getQueryString(url,name)
+{
+    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+    var r = url.match(reg);
+    if(r!=null)return  unescape(r[2]); return null;
+}
+
 module.exports = router;
