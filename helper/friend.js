ad054738bc3ee8374d1992250fe497b905b39c66
juaner <591024170@qq.com>
2017-12-11 16:36:01 +0800

    关注人和被关注人列表

---
+++ helper/friend.js
@@ -0,0 +1,18 @@
+var sqlite3 = require('sqlite3');
+
+var location='/Users/juaner/IdeaProjects/album/database/photo.db';
+exports.getFollowing= function (name,callback) {
+    var db=new sqlite3.Database(location);
+    db.all("SELECT * from user where name in( select name FROM follow where follower=?)",[name],function (err,row){
+        callback(row);
+    })
+    db.close();
+}
+
+exports.getFollower= function (name,callback) {
+    var db=new sqlite3.Database(location);
+    db.all("SELECT * from user where name in( select follower FROM follow where name=?)",[name],function (err,row){
+        callback(row);
+    })
+    db.close();
+}
--- routes/friend.js
+++ routes/friend.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var db=require('../helper/friend');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('friend');
 });
+router.post('/following', function(req, res, next) {
+    var name=req.body.username;
+    var names=[];
+    var pros=[];
 
+    db.getFollowing(name,function (friend) {
+        for(var i=0;i<friend.length;i++){
+            names[i]=friend[i].name;
+            pros[i]=friend[i].profile;
+        }
+        res.json({names:names,pros:pros});
+    })
+
+});
+router.post('/follower', function(req, res, next) {
+    var name=req.body.username;
+    var names=[];
+    var pros=[];
+
+    db.getFollower(name,function (friend) {
+        for(var i=0;i<friend.length;i++){
+            names[i]=friend[i].name;
+            pros[i]=friend[i].profile;
+        }
+        res.json({names:names,pros:pros});
+    })
+
+});
 module.exports = router;
